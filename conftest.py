8d641d12cf23da7fc206ba1e267593854ca0382d
Maksim Starostenko <maksim.starostenko.92@gmail.com>
2025-12-20 16:03:52 +0200

    Added changes in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,16 +1,39 @@
 import pytest
+import tempfile
 from selenium import webdriver
+from selenium.webdriver.chromium.options import ChromiumOptions
+
 
 @pytest.fixture
 def driver():
     """Фикстура для инициализации и закрытия браузера."""
-    options = webdriver.ChromeOptions()
-    # Оставить окно открытым по желанию (можно убрать, если не нужно)
-    options.add_experimental_option("detach", True)
-    # ВРЕМЕННЫЙ workaround вместо maximize_window()
-    # driver.maximize_window()
-    driver = webdriver.Chrome(options=options)
-    driver.set_window_size(1920, 1080)
+    chrome_options = ChromiumOptions()
+    chrome_options.add_experimental_option('detach', True)
+    chrome_options.add_argument('--lang=en')
+    user_data_dir = tempfile.mkdtemp(prefix="selenium-chrome-profile-")
+    chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
+    chrome_options.add_argument('--no-first-run')
+    chrome_options.add_argument('--no-default-browser-check')
+    chrome_options.add_argument('--disable-sync')
+    chrome_options.add_argument('--incognito')
+
+    prefs = {
+        "profile.default_content_setting_values.notifications": 2,
+        "translate": {"enable": False},
+        "intl.accept_languages": "en,en_US",
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "password_manager_leak_detection": False
+    }
+    chrome_options.add_experimental_option('prefs', prefs)
+
+    chrome_options.add_argument(
+        "--disable-features=PasswordLeakDetection,PasswordManagerOnboarding,EnablePasswordsAccountStorage")
+    chrome_options.add_argument("--disable-save-password-bubble")
+    chrome_options.add_argument("--disable-extensions")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_window_size(1440, 860)
 
     yield driver
-    driver.quit()
+    #driver.quit()
