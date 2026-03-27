179c41865cbf23ec05f272cbb32d75029d1f3786
Maksim Starostenko <maksim.starostenko.92@gmail.com>
2026-03-27 18:32:35 +0200

    Added option excludeSwitches enable-logging

--- conftest.py
+++ conftest.py
@@ -13,7 +13,7 @@ def driver():
 
     options.add_argument("--incognito")
     options.add_argument("--disable-notifications")
-
+    options.add_experimental_option("excludeSwitches", ['enable-logging'])
     options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False
