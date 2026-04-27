012c20f624f40ef9c799297e8fd1bbd0f75859c3
Maksim Starostenko <maksim.starostenko.92@gmail.com>
2026-04-27 15:22:37 +0300

    Refactoring base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,26 +5,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from config.settings import DEFAULT_TIMEOUT
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import TimeoutException
+from typing import Tuple
+
 
 class BasePage:
     def __init__(self, driver: WebDriver, timeout: int = DEFAULT_TIMEOUT):
         self.driver = driver
         self.wait = WebDriverWait(driver, timeout)
 
+    """Method open current url"""
     def open(self, url: str) -> None:
         self.driver.get(url)
 
-    def scroll_to(self, locator):
+    """Method scroll to target element"""
+    def scroll_to(self, locator: Tuple[str, str]) -> None:
         element = self.find_present(locator)
         self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
 
-    def find_visible(self, locator) -> WebElement:
+    """Method find visible element"""
+    def find_visible(self, locator: Tuple[str, str]) -> WebElement:
         return self.wait.until(EC.visibility_of_element_located(locator))
 
-    def find_present(self, locator) -> WebElement:
+    """Method find present element"""
+    def find_present(self, locator: Tuple[str, str]) -> WebElement:
         return self.wait.until(EC.presence_of_element_located(locator))
 
-    def click(self, locator, retries: int = 3):
+    """Method click element"""
+    def click(self, locator: Tuple[str, str], retries: int = 3) -> None:
         last_exception = None
 
         for attempt in range(retries):
@@ -38,7 +45,8 @@ class BasePage:
                     # если все попытки провалились
                     raise last_exception
 
-    def click_if_present(self, locator, timeout: int = 3) -> bool:
+    """Method click if present"""
+    def click_if_present(self, locator: Tuple[str, str], timeout: int = 3) -> bool:
         try:
             wait = WebDriverWait(self.driver, timeout)
             element = wait.until(EC.element_to_be_clickable(locator))
@@ -47,16 +55,19 @@ class BasePage:
         except TimeoutException:
             return False
 
-    def type(self, locator, text: str, clear: bool = True) -> None:
+    """Method type text if element is visible"""
+    def type(self, locator: Tuple[str, str], text: str, clear: bool = True) -> None:
         el = self.find_visible(locator)
         if clear:
             el.clear()
         el.send_keys(text)
 
-    def text(self, locator) -> str:
+    """Method GET text from locator"""
+    def text(self, locator: Tuple[str, str]) -> str:
         return self.find_visible(locator).text
 
-    def is_visible(self, locator) -> bool:
+    """Method GET visible element"""
+    def is_visible(self, locator: Tuple[str, str]) -> bool:
         try:
             self.find_visible(locator)
             return True
